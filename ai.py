5eebafd7b46d546d88c85e8facbc1a2be93898d1
abhishek-karmakar <abhishek.karmakar.micromaxinfo.com>
2018-02-20 15:59:41 +0530

    adding circleci

--- ai.py
+++ ai.py
@@ -30,9 +30,21 @@ class Network(nn.Module):
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30) #Full connection between input layer to all the input of hidden layer. 
-        self.fc2 = nn.Linear(30, nb_actions)
+        self.fc2 = nn.Linear(30, nb_action)
     
     def forward(self, state) :
         x = F.relu(self.fc1(state)) # the first full connection of the input neurons
         q_values = self.fc2(x) #get the output Q values of the Neural network
-        return q_values # return the Q values. 
+        return q_values # return the Q values.
+    
+#Imlement Experience Replay
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = [] # An empty list. A simple list with 100 elements
+        
+    def push(self, event):
+        self.memory.append(event) #it will append the last event in the memory.
+        if len(self.memory) > self.capacity:
+            del self.memory[0] # delete the first 
+        
